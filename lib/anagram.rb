10ab8929e6c5b5b65ee92a33b055b39e09ce039e
rmaghsoudi <romy_mag@outlook.com>
2019-04-29 17:03:05 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
-# Your code goes here!
+require 'pry'
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)
+    self_letters = self.word.split("").sort
+    array.select {|e| self_letters == e.split("").sort && self.word.length == e.length}
+  end
+
+end
