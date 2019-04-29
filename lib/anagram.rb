74af1a37c778d30a38eabd2897ab1bdc43bb27aa
Andrew Allen <asallen67@gmail.com>
2019-04-29 16:02:29 -0400

    cleaned

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,22 +1,8 @@
-# Your code goes here!
-require "pry"
 class Anagram
+
 	def initialize(word)
 		@word = word
 	end
-
-	# def match(string)
-	# 	check = true
-	# 	anagrams = []
-	# 	string.each {|str|
-	# 		if @word.length == str.length
-	# 			str.each {|char|
-	# 				check = false if !word.include?(char)
-	# 			}
-	# 			anagrams << str if check
-	# 		end
-	# 	}
-	# end
 
 	def match(string_array)
 		is_anagram = true
@@ -33,7 +19,6 @@ class Anagram
 				all_anagrams << string if is_anagram
 				is_anagram = true
 			end
-			# binding.pry
 		}
 		all_anagrams
 	end
